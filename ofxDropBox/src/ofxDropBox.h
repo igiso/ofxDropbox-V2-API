5e2a877e3e38590fa62056a38a49068628078115
kyriacos kousoulides <gli_koullis@msn.com>
2019-11-06 16:10:45 +0200

    Update ofxDropBox.h

--- ofxDropBox/src/ofxDropBox.h
+++ ofxDropBox/src/ofxDropBox.h
@@ -14,31 +14,7 @@ extern bool GO_BACK_TO_LOAD_AGAIN_EVERYTHING;
 
 /*
  ofxDropBox Originally created by Trent Brooks.
-    - original dropbox ios tutorial: https://www.dropbox.com/developers/start/setup#ios
  
-    Steps to setup DropBox SDK: 
-    1. create a dropbox app on https://www.dropbox.com/developers/apps
-    2. download the dropbox sdk: https://www.dropbox.com/developers/reference/sdk
-        - once downloaded, drag and drop the 'DropboxSDK.framework' onto your OF xcode project
-        - make sure you check the 'copy items into destination group's folder' checkbox.
-    3. add the 'Security.framework' to your OF xcode project
-        - Under TARGETS > ofxDropBoxExample, under the 'Build Phases' tab, where it says 'Link binary with libraries', click '+' and select the 'Security.framework'.
-    4. copy your dropbox app key + secret from dropbox website
-        - ref: https://www.dropbox.com/developers/start/authentication#ios
-    5. Edit the ofxiphone-info.plist in your OF xcode project (right click open as source code)
-        - make sure the UIApplicationExitsOnSuspend is set to false 
-        - Add the following code at the top after the first <dict> tag and replace 'db-APP_KEY' with your app key eg. 'db-n0vfoe7o3a5bmtz'.
-        <key>CFBundleURLTypes</key>
-        <array>
-            <dict>
-                <key>CFBundleURLSchemes</key>
-                <array>
-                    <string>db-APP_KEY</string>
-                </array>
-            </dict>
-        </array>
-    6. If testing with the ofxDropBoxExample project. Remember to copy 'test.jpg' to your DropBox/Apps/YourApp folder otherwise the download won't work.
-    7. That should be it. See the example testApp to see how to connect, upload, and download using ofxDropBox.
  */
 /*revised by Kyriacos Kousoulides to support V2 api*/
 
@@ -52,7 +28,6 @@ extern string DOWNLOAD_IN_PROCCESS,TARGET_PATH_TO_UPLOAD,LISTED_DIRECTORY;
 
 
 
-// OF CLASS
 class ofxDropBox : public ofxiPhoneAlertsListener
 {
 
@@ -61,8 +36,8 @@ public:
     ofxDropBox();
     virtual ~ofxDropBox();
     
-    // ios delegate
-   // ofxDropBoxDelegate* dropBoxDelegate; 
+   
+ 
     
     void startSession(string appKey, string appSecret, bool useDBRootAppFolder = true);
     
@@ -95,6 +70,7 @@ public:
     ofEvent<bool> onQueueUploadEvent; // notifies when a whole queue has uploaded instead of individually
     ofEvent<bool> onQueueDownloadEvent;
     
+ //list a directory
     vector<string> & listDirectory(string f="/");
     string loadedPath;
     DBUserClient *client;
